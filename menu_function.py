4772a51a049ab949e3a896513b49ec82ade7ee72
Quang-Kusho <thienquang0511@gmail.com>
2023-12-07 17:32:04 +0200

    first assigment

--- menu_function.py
+++ menu_function.py
@@ -3,7 +3,7 @@ import calendar
 from datetime import datetime as dt
 
 def get_time():
-    datetime_str = input("Enter the date and time in the format 'dd.mm.yyyy hh:mm':\n")
+    datetime_str = input("Enter the date and time in the format 'dd.mm.yyyy hh:mm': \n")
     datetime_object = dt.strptime(datetime_str, '%d.%m.%Y %H:%M')
     for i in ["year", "month", "day", "hour", "minute"]:
         print("You gave year " + str(eval(f"datetime_object.{i}")))
